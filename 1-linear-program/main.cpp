3d3d2805a7ca7e5a3a78d833ed3387c5da44a1a0
Robakiller <43248130+Robakiller@users.noreply.github.com>
2018-10-03 20:22:32 +0300

    Update with cmath

--- 1-linear-program/main.cpp
+++ 1-linear-program/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 #define PI	3.14159265358979323846
 
@@ -12,8 +12,10 @@ int main()
 	cout << "Enter the angle in degrees: ";
 	cin >> vdAngle;
 
-	cout << "Your result of solution with the first formula is " << 1 - 0.25*pow(sin(2*vdAngle * PI / 180), 2) + cos(2*vdAngle * PI / 180) << endl;
-	cout << "Of course, your result of solution with the second formula is " << pow(cos(vdAngle * PI / 180), 2) + pow(cos(vdAngle * PI / 180), 4) << " too" << endl;
+	cout << "Your result of solution with the first formula is ";
+	cout << 1 - 0.25*pow(sin(2*vdAngle * PI / 180), 2) + cos(2*vdAngle * PI / 180) << endl;
+	cout << "Of course, your result of solution with the second formula is ";
+	cout << pow(cos(vdAngle * PI / 180), 2) + pow(cos(vdAngle * PI / 180), 4) << " too" << endl;
 
 	system("PAUSE");
 
