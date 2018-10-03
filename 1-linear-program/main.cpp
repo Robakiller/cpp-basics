8c3911c28be968c7da1ae6eb972fe1708aaa50af
Robakiller <43248130+Robakiller@users.noreply.github.com>
2018-10-03 20:35:53 +0300

    Update 2

--- 1-linear-program/main.cpp
+++ 1-linear-program/main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <cmath>
 
-#define PI	3.14159265358979323846
+#define PI 3.14159265358979323846
 
 using namespace std;
 
 int main()
 {
-	double vdAngle;
+ double vdAngle;
 
-	cout << "Enter the angle in degrees: ";
-	cin >> vdAngle;
+ cout << "Enter the angle in degrees: ";
+ cin >> vdAngle;
 
-	cout << "Your result of solution with the first formula is ";
-	cout << 1 - 0.25*pow(sin(2*vdAngle * PI / 180), 2) + cos(2*vdAngle * PI / 180) << endl;
-	cout << "Of course, your result of solution with the second formula is ";
-	cout << pow(cos(vdAngle * PI / 180), 2) + pow(cos(vdAngle * PI / 180), 4) << " too" << endl;
+ cout << "Your result of solution with the first formula is ";
+ cout << 1 - 0.25*pow(sin(2*vdAngle * PI / 180), 2) + cos(2*vdAngle * PI / 180);
+ cout << "\nOf course, your result of solution with the second formula is ";
+ cout << pow(cos(vdAngle * PI / 180), 2) + pow(cos(vdAngle * PI / 180), 4) << " too";
 
-	system("PAUSE");
-
-	return 0;
+ return 0;
 }
